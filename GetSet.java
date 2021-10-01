fa64ae3cabe365922d682a17f8e59a16d20ce74a
Casoco, Marc T <77011668+marcthe5@users.noreply.github.com>
2021-10-01 11:25:43 +0800

    Add files via upload

---
+++ GetSet.java
@@ -0,0 +1,12 @@
+package MAC;
+
+public class GetSet {
+
+	private String userPhone;
+	public void setPhone(String phone) {
+		userPhone = phone;
+	}
+	public String getPhone() {
+		return userPhone;
+	}
+}
